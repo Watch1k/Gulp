9aeacba7736cee2ac52fc762433f5cb0883a1a4e
watch1k <host.sf@gmail.com>
2016-04-30 04:18:30 +0300

    placeholder

--- src/js/common.js
+++ src/js/common.js
@@ -2,11 +2,12 @@ head.ready(function(){
 
 	// Clear placeholder
 	(function() {
-		$('input,textarea').focus(function(){
-				$(this).data('placeholder',$(this).attr('placeholder'))
+		var el = $('input, textarea');
+		el.focus(function(){
+				$(this).data('placeholder',$(this).attr('placeholder'));
 				$(this).attr('placeholder','');
 		});
-		$('input,textarea').blur(function(){
+		el.blur(function(){
 			$(this).attr('placeholder',$(this).data('placeholder'));
 		});
 	}());

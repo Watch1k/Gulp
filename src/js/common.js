351a228f196ecc3a005924cb07f3b763ad82c62f
watch1k <host.sf@gmail.com>
2016-05-02 21:28:00 +0300

    update

--- src/js/common.js
+++ src/js/common.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
 	(function() {
 		var el = $('input, textarea');
 		el.focus(function(){
-				$(this).data('placeholder',$(this).attr('placeholder'));
-				$(this).attr('placeholder','');
+			$(this).data('placeholder',$(this).attr('placeholder'));
+			$(this).attr('placeholder','');
 		});
 		el.blur(function(){
 			$(this).attr('placeholder',$(this).data('placeholder'));

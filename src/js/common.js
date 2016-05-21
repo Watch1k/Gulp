9d28415422229e3f7532b2888b00cc9b1b5b1d02
watch1k <host.sf@gmail.com>
2016-05-21 23:38:09 +0300

    upd

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,23 @@
 /* Common JS */
 $(document).ready(function(){
 
+	// detect phone/tablet
+	var md = new MobileDetect(window.navigator.userAgent);
+	console.log(md.phone());
+	console.log(md.tablet());
+
+	// check orientation device
+	function isPortrait() {
+		return window.innerHeight > window.innerWidth;
+	};
+
+	// Listen for orientation changes
+	window.addEventListener("orientationchange", function() {
+		if (isPortrait()) {
+			// code here
+		}
+	}, false);
+
 	// Clear placeholder
 	(function() {
 		var el = $('input, textarea');
